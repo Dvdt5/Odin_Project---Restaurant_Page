86613a4ceead7926a096450bb356ffc77d3d0c86
Davut Dagtekin <dagtekindavut07@gmail.com>
2024-09-04 15:09:28 +0300

    Made animation for content appearing

--- src/home.js
+++ src/home.js
@@ -4,5 +4,6 @@ export default function home () {
     const homeHeaderText = document.createElement("p");
     homeHeaderText.id = "head-text-home";
     homeHeaderText.innerHTML = "DeepLake Restaurant <br><br><span>World's Best Seafood Restaurant</span>";
+    homeHeaderText.classList.add("fadeAnimation");
     return homeHeaderText;
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import home from "./home";
 const homeButton = document.getElementById("home-page-logo");
 const mainContent = document.getElementById("main");
 
+
 homeButton.addEventListener("click", ()=>{
     mainContent.innerHTML = "";
-    mainContent.appendChild(home()); 
+    mainContent.appendChild(home());
 });
 
-window.onload = ()=> mainContent.appendChild(home()); ;
+//window.onload = ()=> mainContent.appendChild(home()); ;
